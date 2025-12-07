0d14c90547d6b5477c5942b04638d2db95b67113
Login <mail@gmail.com>
2025-12-07 17:13:28 +0100

    lesson04_ifelse

---
+++ lesson04_ifelse/lesson4.js
@@ -0,0 +1,13 @@
+// const titleProject = "Оценка стоимости";
+// const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+// const screenPrice = 100;
+// const percentage = 10;
+// const fullPrice = 200;
+// const responsive = true;
+
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 100;
+let percentage = 10;
+let fullPrice = 200;
+let responsive = true;
