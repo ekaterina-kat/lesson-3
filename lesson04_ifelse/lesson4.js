8e92448d745000b4691afcbc00ce0da615006590
Login <mail@gmail.com>
2025-12-07 18:13:45 +0100

    lesson04_ifelse homework

--- lesson04_ifelse/lesson4.js
+++ lesson04_ifelse/lesson4.js
@@ -5,9 +5,43 @@
 // const fullPrice = 200;
 // const responsive = true;
 
-let titleProject = "Оценка стоимости";
-let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
-let screenPrice = 100;
+let titleProject = prompt("Название проекта?");
+// console.log(titleProject);
+
+let screensValue = prompt(
+  "Какие типы экранов: шаблонные, с уникальным дизайном, с анимациями"
+);
+let screenPrice = 10000;
 let percentage = 10;
-let fullPrice = 200;
-let responsive = true;
+let fullPrice = 200000;
+let responsive = Boolean(prompt("нужен ли респонсивный сайт: true / false?"));
+// console.log(responsive);
+// console.log(typeof responsive);
+
+let service1 = prompt("Какой сервис нужен");
+let servicePrice1 = +prompt("Сколько это будет стоить");
+// console.log(typeof servicePrice1);
+let service2 = prompt("Какой еще сервис тебе нужен");
+let servicePrice2 = +prompt("Сколько будет стоить этот второй сервис?");
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.round(
+  fullPrice - (fullPrice / 100) * percentage
+);
+
+// console.log(fullPrice);
+if (fullPrice > 50000) {
+  console.log("скидка 10%");
+} else if (fullPrice === 50000) {
+  console.log("скидка 9%");
+} else if (fullPrice > 20000 && fullPrice < 50000) {
+  console.log("сделаем скидку 5%");
+} else if (fullPrice === 20000) {
+  console.log("скидка 4%");
+} else if (fullPrice > 0 && fullPrice < 20000) {
+  console.log("скидка не предусмотрена");
+} else if (fullPrice === 0) {
+  console.log("Ошибка: стоимость не может быть равна нулю!");
+} else if (fullPrice < 0) {
+  console.log("Что-то пошло не так");
+}
