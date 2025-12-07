8ec77552136d5a9e89962400e4239252eeba66c6
Login <mail@gmail.com>
2025-12-07 16:08:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const titleProject = "Оценка стоимости";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 100;
+const percentage = 10;
+const fullPrice = 200;
+const responsive = true;
